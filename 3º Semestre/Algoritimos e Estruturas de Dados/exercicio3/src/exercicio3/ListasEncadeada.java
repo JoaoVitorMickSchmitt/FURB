d19851179af42361761fb4af90dfb87413559dd4
JoaoVitorMickSchmitt <joaovitormickschmitt@gmail.com>
2026-03-05 21:01:36 -0300

    exercicio3

---
+++ 3º Semestre/Algoritimos e Estruturas de Dados/exercicio3/src/exercicio3/ListasEncadeada.java
@@ -0,0 +1,19 @@
+package exercicio3;
+
+public class ListasEncadeada {
+
+	private NoLista primeiro;
+
+	public ListasEncadeada(NoLista primeiro) {
+		super();
+		this.primeiro = primeiro;
+	}
+	
+public void inserir(int info) {
+	NoLista novo = new NoLista();
+	novo.setInfo(info);
+}
+	
+
+	
+}
